22a2d80dd26fc0612b98fe67672ffb404bee8ea8
Bruno Jaime <brunojaime@users.noreply.github.com>
2026-06-10 14:23:46 -0300

    Add native audio recording to feedback template

--- packages/codex_developer_feedback_template/lib/developer_feedback_audio_recorder.dart
+++ packages/codex_developer_feedback_template/lib/developer_feedback_audio_recorder.dart
@@ -1,2 +1,3 @@
 export 'developer_feedback_audio_recorder_stub.dart'
-    if (dart.library.html) 'developer_feedback_audio_recorder_web.dart';
+    if (dart.library.html) 'developer_feedback_audio_recorder_web.dart'
+    if (dart.library.io) 'developer_feedback_audio_recorder_io.dart';
